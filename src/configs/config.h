53386c1d40a04f3a5c564d2ba9d3da1004daac1a
ekartashov <ekartashov@ozon.ru>
2025-05-30 16:43:36 +0300

    make secrets

--- src/configs/config.h
+++ src/configs/config.h
@@ -1,16 +1,12 @@
 #pragma once
 
 #define F_VERSION "1.0"
-#define AP_NAME "AirQualityMonitor AP"
+#define AP_NAME "AirQualityMonitor AP" // Prefix for wi-fi point with settings
 #define AP_PASS "admin"
 
 #define MQTT_SERVER "m8.wqtt.ru"
 #define MQTT_PORT 20336
-#define MQTT_USERNAME "u_7DGM4U"
-#define MQTT_PASS "7jID9K7G"
 
-#define WIFI_SSID "MERCUSYS_0544"
-#define WIFI_PASS "11447887"
 #define WIFI_CONN_RETRY_COUNT 20
 #define WIFI_CONN_RETRY_TIMEOUT 500 // ms
 
---
+++ src/configs/secrets.example.h
@@ -0,0 +1,4 @@
+#define WIFI_SSID "*****"
+#define WIFI_PASS "*****"
+#define MQTT_USERNAME "*****"
+#define MQTT_PASS "*****"
