b181ce7939610058cf02e8ff231cc33195dcb8f1
ekartashov <ekartashov@ozon.ru>
2025-07-26 16:35:35 +0300

    feat: minor

---
+++ src/model/sensor_state.h
@@ -0,0 +1,10 @@
+/**
+ * @enum Sensor_State
+ * @brief Enumeration for sensor states
+ */
+typedef enum
+{
+    Sensor_INIT,       ///< Initialization state
+    Sensor_RUNNING,    ///< Normal operation state
+    Sensor_CALIBRATING ///< Calibration state
+} Sensor_State;
